503552d466daa624fc591b6d7d3b944344ebca93
Pawel Mika <pawel.mika@intive.com>
2024-10-27 14:48:03 +0100

    Blink + test drag

--- Assets/Scripts/PlanetAttack/ThePlanet/MainPlanet.cs
+++ Assets/Scripts/PlanetAttack/ThePlanet/MainPlanet.cs
@@ -1,12 +1,15 @@
 using System;
+using System.Drawing;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using Color = UnityEngine.Color;
 using Random = UnityEngine.Random;
 
 namespace PlanetAttack.ThePlanet
 {
-    public class MainPlanet : MonoBehaviour
+    public class MainPlanet : MonoBehaviour, IDragHandler, IInitializePotentialDragHandler
     {
         private float rotationPerSec = 0.05f;
         public float RotationPerSec
@@ -69,6 +72,8 @@ namespace PlanetAttack.ThePlanet
         {
             Planet.transform.Rotate(Vector3.up, this.rotationSpeed * Time.deltaTime);
             CheckClicked();
+
+            BlinkPlayerHalo();
         }
 
         private void CheckClicked()
@@ -94,6 +99,47 @@ namespace PlanetAttack.ThePlanet
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             Debug.DrawRay(ray.origin, ray.direction, Color.magenta);
+        }
+
+        private void BlinkPlayerHalo() {
+            Material mp = PlayerPlanetHalo.GetComponent<Renderer>().material;
+            float cp = 1f + (float)(Math.Sin(Utils.Sawtooth(Time.time, 1, 0f, 1f)) * 6f);
+            mp.SetFloat("_Falloff", cp);
+
+            Transform tPlayer = PlayerPlanetHalo.transform;
+            float scale = 1f + (float)Math.Sin(Utils.Sawtooth(Time.time, 1, 0.25f, 1.25f));
+            tPlayer.localScale = new Vector3(scale, scale, scale);
+        }
+
+        Vector3 dragStartPoint;
+
+        public void OnBeginDrag() {
+            dragStartPoint = transform.position;
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            // Vector3 delta = eventData.pointerPressRaycast.worldPosition - eventData.pointerCurrentRaycast.worldPosition;
+            // Vector3 delta = eventData.pointerCurrentRaycast.worldPosition;
+            // Vector3 delta = Camera.main.WorldToViewportPoint(eventData.pointerPressRaycast.worldPosition) - Camera.main.WorldToViewportPoint(eventData.pointerCurrentRaycast.worldPosition);
+            
+            // Vector3 delta = dragStartPoint - Camera.main.WorldToViewportPoint(eventData.pointerCurrentRaycast.worldPosition);
+            // delta.z = 0;
+            // Debug.Log(delta);
+            // transform.position += delta;
+
+            Ray R = Camera.main.ScreenPointToRay(Input.mousePosition); // Get the ray from mouse position
+            Vector3 PO = transform.position; // Take current position of this draggable object as Plane's Origin
+            Vector3 PN = -Camera.main.transform.forward; // Take current negative camera's forward as Plane's Normal
+            float t = Vector3.Dot(PO - R.origin, PN) / Vector3.Dot(R.direction, PN); // plane vs. line intersection in algebric form. It find t as distance from the camera of the new point in the ray's direction.
+            Vector3 P = R.origin + R.direction * t; // Find the new point.
+
+            transform.position = P;
+        }
+
+        public void OnInitializePotentialDrag(PointerEventData eventData)
+        {
+            eventData.useDragThreshold = false;
         }
     }
 }
--- Assets/Scripts/PlanetAttack/Utils.cs
+++ Assets/Scripts/PlanetAttack/Utils.cs
@@ -1,4 +1,7 @@
+using System;
+using UnityEditor;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace PlanetAttack
 {
@@ -7,6 +10,16 @@ namespace PlanetAttack
         public static Vector3 GetRandomVectorInRange(float fromX, float toX, float fromY, float toY)
         {
             return new Vector3(Random.Range(fromX, toX), Random.Range(fromY, toY));
+        }
+
+        static double Fract(double t)
+        {
+            return t - Math.Floor(t);
+        }
+
+        public static double Sawtooth(double t, double period, double min, double max)
+        {
+            return min + (max - min) * Fract(t / period);
         }
     }
 
